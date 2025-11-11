d20e1ea74bc2bd6f3c89eaa90c76cc67d5d0ac9a
Shruti Rai <shrutir556@gmail.com>
2025-11-12 00:52:17 +0530

    Gamebox

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
-var main =document.getElementById("main");
-var arr = ["https://i.pinimg.com/736x/90/17/c2/9017c20aeb8bbe2417235023bb69455b.jpg",
-"https://i.pinimg.com/736x/81/60/5e/81605e77565564c36a62eb8c3c829997.jpg",
-"https://i.pinimg.com/736x/c3/b3/f1/c3b3f1ea25dbf06e84b214751667cbee.jpg",
-"https://i.pinimg.com/736x/47/91/d3/4791d3c30033aedcb4fd29a2c104e398.jpg",
-"https://i.pinimg.com/736x/e7/18/11/e718112f06983f23ea6c43a01db55404.jpg",
-"https://i.pinimg.com/1200x/96/79/85/967985ca22b2b00b485eca2bd47295d2.jpg",
-"https://i.pinimg.com/736x/ee/dd/74/eedd740497705d3df314b21d6c9334ab.jpg",
-"https://i.pinimg.com/736x/65/14/e2/6514e2ade7ba2eddff40960d1ec8f507.jpg"];
-let s = "";
-for(let i =1; i<40;i++){
-    let r = Math.floor(Math.random()*arr.length);
-    s += `<div class="card"><img src=${arr[r]}>
-    </div>`;
-}
-main.innerHTML = s;
+let s1= document.getElementById("sq1");
+let s2= document.getElementById("sq2");
+let s3= document.getElementById("sq3");
+let s4= document.getElementById("sq4");
+s1.addEventListener("mouseenter", function(){
+    let r =Math.floor(Math.random()*100);
+    s1.innerHTML="<h1>"+r+"</h1>";
+   
+})
+s1.addEventListener("mouseleave", function(){
+   s1.innerHTML="<h1>1</h1>";
+})
+let clr ="red";
+s2.addEventListener("mouseenter", function(){
+    if(clr=="green"){
+   s2.style.backgroundColor = "green";
+   clr="red";
+    }
+    else if(clr=="red"){
+        s2.style.backgroundColor = "red";
+        clr ="blue";
+    }
+    else{
+        s2.style.backgroundColor = "blue";
+        clr ="green";
+    }
+})
+s2.addEventListener("mouseleave", function(){
+   s2.style.backgroundColor = "white";
+})
 
+s3.addEventListener("mouseenter", function(){
+     let r1 =Math.floor(Math.random()*150);
+    s3.style.backgroundColor= r1;
+     let r2 =Math.floor(Math.random()*250);
+    s3.style.backgroundColor= r2;
+     let r3 =Math.floor(Math.random()*300);
+    s3.style.backgroundColor= r3;
+    s3.style.backgroundColor =`rgb(${r1},${r2},${r3})`;
+})
+s3.addEventListener("mouseleave", function(){
+    s3.style.backgroundColor = "white";
+})
+
+s4.addEventListener("mouseleave", function(){
+ s1.style.backgroundColor = "white";
+  s2.style.backgroundColor = "white";
+   s3.style.backgroundColor = "white";
+})
+s4.addEventListener("mouseenter", function(){
+let r1 =Math.floor(Math.random()*250);
+let r2 =Math.floor(Math.random()*250);
+let r3 =Math.floor(Math.random()*250);
+s1.style.backgroundColor =`rgb(255,${r3},0)`;
+s2.style.backgroundColor =`rgb(0,255,${r3})`;
+s3.style.backgroundColor =`rgb(${r1},${r2},255)`;
+})
